696bd409dad050e946ca79826645b0730ddd7216
hakim-asrori <saneglos005@gmail.com>
2025-11-17 10:52:50 +0700

    update

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,4 +1,3 @@
-import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import {
@@ -22,9 +21,7 @@ import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
 import {
     ArrowLeftRightIcon,
-    BookOpen,
     DownloadCloudIcon,
-    Folder,
     LayoutDashboardIcon,
     ListCheckIcon,
     ShoppingCartIcon,
@@ -96,19 +93,6 @@ const mainNavItems: NavItem[] = [
     },
 ];
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
-];
-
 export function AppSidebar() {
     return (
         <Sidebar collapsible="icon" variant="floating">
@@ -129,7 +113,6 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
